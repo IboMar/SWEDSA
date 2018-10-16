2d9f509d20c1e8bbb2d59c3f7065f885c1cd474f
cgori <36439786+cgori@users.noreply.github.com>
2018-10-16 18:11:11 +0100

    Project creation-

---
+++ Stop&Search/src/Main.java
@@ -0,0 +1,49 @@
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+
+	public static void main(String[] args) throws IOException {
+		ArrayList<CrimeStopAndsearch> CrimeStopAndsearch = new ArrayList();
+		
+		CSVFiles.readInFilesDir();
+		List<String> xd = FolderReader.getbasicStop_Search();
+		for(String temp : xd) {
+			
+			CrimeStopAndsearch = Data_Handling.readFile(temp);
+		}
+
+		//ArrayList<Crime> crime = Data_Handling.readFile("Data\\crimedata-full-month.csv");
+		Scanner scan = new Scanner(System.in);
+		System.out.println("a. By Crime type (lexicographic order)\r\n"
+				+ "b. By Last outcome category (again, lexicographic order)\r\n"
+				+ "c. By LSOA Name (this is as presented in the file)" + "");
+		String Menu = scan.nextLine();
+
+		switch (Menu) {
+		case "a":
+			//Data_Manipulation.crime_Lexicographic_order(CrimeStopAndsearch);
+			Data_Handling.outputCrimes(CrimeStopAndsearch);
+			break;
+
+		case "b":
+			//Data_Manipulation.category_Lexicographic_order(CrimeStopAndsearch);
+			Data_Handling.outputCrimes(CrimeStopAndsearch);
+			break;
+
+		case "c":
+			//Data_Manipulation.LSOA_Name(CrimeStopAndsearch);
+			Data_Handling.outputCrimes(CrimeStopAndsearch);
+			break;
+
+		}
+	}
+}
